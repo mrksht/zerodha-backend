2909d752403393f4b5461707f0e441e9eb0c1235
Rakshit <rakshit@Rakshits-MacBook-Pro.local>
2025-05-14 22:40:10 +0530

    added cors types

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "https://zerodha-dashboard-mu.vercel.app/", credentials: true }));
+app.use(cors({ origin: "https://zerodha-dashboard-mu.vercel.app", credentials: true }));
 app.use(express.json());
 
 app.use("/", portfolioRoutes);
